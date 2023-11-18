ab95ac173b2d7affc3291f4fb981c2fb7b87f83a
Kaven <kaven@wuwenkai.com>
2023-11-18 20:33:15 +0800

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,11 @@
  * @website:     http://api.kaven.xyz
  * @file:        [Kaven-Public-API] /.eslintrc.js
  * @create:      2022-06-27 14:22:02.898
- * @modify:      2022-06-27 14:22:02.898
+ * @modify:      2023-11-18 20:29:43.942
  * @version:     0.0.2
- * @times:       1
- * @lines:       16
- * @copyright:   Copyright © 2022 Kaven. All Rights Reserved.
+ * @times:       2
+ * @lines:       32
+ * @copyright:   Copyright © 2022-2023 Kaven. All Rights Reserved.
  * @description: [description]
  * @license:     [license]
  ********************************************************************/
@@ -27,5 +27,6 @@ module.exports = {
         ecmaVersion: 13,
     },
     rules: {
+        console: "allow",
     },
 };
